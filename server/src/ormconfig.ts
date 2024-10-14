3515e0a27919e7b7cbe29a285b652080c84259ea
tonwayer <tonwayer@gmail.com>
2024-10-14 13:39:17 -0700

    [client, server] add Dockerfiles

--- server/src/ormconfig.ts
+++ server/src/ormconfig.ts
@@ -3,11 +3,11 @@ import { QuestionSubscriber } from './subscribers/QuestionSubscriber';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
+    host: `${process.env.POSTGRES_HOST || 'localhost'}`,
     port: 5432,
-    username: 'postgres',
-    password: '123',
-    database: 'postgres',
+    username: `${process.env.POSTGRES_USER || 'postgres'}`,
+    password: `${process.env.POSTGRES_PASSWORD || 123}`,
+    database: `${process.env.POSTGRES_DB || 'postgres'}`,
     synchronize: true,
     logging: false,
     entities: ['src/entities/**/*.ts'],
